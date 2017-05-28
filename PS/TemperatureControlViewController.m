065bdac88220c119c3fb2766063de8b428a12f79
qifeiqin <qifeiqin@gmail.com>
2017-05-28 19:21:07 +0800

    设置SP

--- PS/TemperatureControlViewController.m
+++ PS/TemperatureControlViewController.m
@@ -34,6 +34,72 @@
     else
         [self setTemperature];
 }
+- (IBAction)setSP:(id)sender {
+    if(0==self.mUITextFieldSP.text.length)
+        [Alert showMessageAlert:@"SP不能为空" view:self];
+    else
+        [self setSP];
+    
+    
+}
+-(void) setSP{
+    AppDelegate *myDelegate = [[UIApplication sharedApplication]delegate];
+    
+    NSString *urlString= [NSString stringWithFormat:@"%@/submitGearboxoil.html",myDelegate.ipString];
+    AFHTTPRequestOperationManager *manager=[AFHTTPRequestOperationManager manager];
+    // manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"application/json", nil];
+    manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"text/html", nil];
+    //pvalue=44&psid=10&pname=升速箱油温SP
+    NSDictionary *parameters = @{
+                                 @"psid":  [DataBaseNSUserDefaults getData:@"selectedPS"],
+                                 @"pname":@"升速箱油温SP",
+                                 @"pvalue":self.mUITextFieldSP.text
+                                 };
+    [manager POST:urlString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        
+        
+        NSString *result=[JsonUtil DataTOjsonString:responseObject];
+        NSLog(@"***************返回结果***********************");
+        NSLog(result);
+        NSData *data=[result dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error=[[NSError alloc]init];
+        NSDictionary *doc= [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        if(doc!=nil){
+            NSLog(@"*****doc不为空***********");
+            //判断code 是不是0
+            if([@"0" isEqualToString:[doc objectForKey:@"code"]])
+            {
+                self.mUILabelSP.text=self.mUITextFieldSP.text;
+                [Alert showMessageAlert:@"设置SP成功" view:self];
+                
+            }
+            else{
+                // [Alert showMessageAlert:[doc objectForKey:@"msg"] view:self];
+                [Alert showMessageAlert:@" 设置失败"view:self];
+            }
+        }
+        else
+            NSLog(@"*****doc空***********");
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSString *errorUser=[error.userInfo objectForKey:NSLocalizedDescriptionKey];
+        
+        if(error.code==-1009)
+            errorUser=@"主人，似乎没有网络喔！";
+        [Alert showMessageAlert:errorUser view:self];
+    }];
+    
+
+    
+    
+    
+    
+}
+
+
+
 
 //设置油温
 -(void) setTemperature{
