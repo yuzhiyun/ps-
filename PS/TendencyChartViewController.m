cdafdde68c6f8313e069ebd12165387d9899079a
qifeiqin <qifeiqin@gmail.com>
2017-06-21 14:56:03 +0800

    找到了ARLineCharts 刷新数据的接口

--- PS/TendencyChartViewController.m
+++ PS/TendencyChartViewController.m
@@ -11,20 +11,35 @@
 #import "AppDelegate.h"
 #import "ARLineChartView.h"
 #import "ARLineChartCommon.h"
-
+#import "AppDelegate.h"
+#import "AFNetworking.h"
+#import "JsonUtil.h"
+#import "Alert.h"
+#import "Parameter.h"
+#import "ParameterInImageViewController.h"
+#import "DataBaseNSUserDefaults.h"
 @interface TendencyChartViewController ()
 
 @property (strong, nonatomic) ARLineChartView *lineChartView;
 @end
 
-@implementation TendencyChartViewController
+@implementation TendencyChartViewController{
+    NSMutableArray *dataSource;
+    int i;
+    CGRect rect;
+    Boolean isChartAddedToSuperView;
+}
 
 - (void)viewDidLoad {
 
     [super viewDidLoad];
-    
+    i=0;
+    isChartAddedToSuperView=false;
     self.title=@"变化趋势";
-    
+    dataSource = [NSMutableArray array];
+    rect = CGRectMake(5, 40,
+                      self.view.frame.size.width - 5 - 5,
+                      self.view.frame.size.height - 30-20-self.tabBarController.tabBar.frame.size.height);
     
     AppDelegate *myDelegate = [[UIApplication sharedApplication]delegate];
     [self.navigationController.navigationBar setBarTintColor:myDelegate.navigationBarColor];
@@ -35,66 +50,8 @@
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     //    修改下一个界面返回按钮的title，注意这行代码每个页面都要写一遍，不是全局的
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"返回" style:UIBarButtonItemStylePlain target:nil action:nil];
-    /*
-    NSMutableArray *xArray = [NSMutableArray array];
-    NSMutableArray *yArray = [NSMutableArray array];
     
-    for (NSInteger i = 0; i < 50; i++) {
-        [xArray addObject:@"06:35"];
-        [yArray addObject:[NSString stringWithFormat:@"%.2lf",50.0+arc4random_uniform(50)]];
-    }
-    
-    double  alignTop=self.navigationController.navigationBar.bounds.size.height+[[UIApplication sharedApplication] statusBarFrame].size.height;
-    
-    double  height=self.view.frame.size.height-self.navigationController.navigationBar.bounds.size.height-[[UIApplication sharedApplication] statusBarFrame].size.height-self.tabBarController.tabBar.bounds.size.height;
-    
-    WSLineChartView *wsLine = [[WSLineChartView alloc]initWithFrame:CGRectMake(0, alignTop, self.view.frame.size.width, height) xTitleArray:xArray yValueArray:yArray yMax:100 yMin:0];
-    
-    
-    [self.view addSubview:wsLine];
-    */
-    [self setCharts];
-    
-}
-#pragma mark 使用ARLineChart
-- (void)setCharts{
-    //************* Test: Create Data Source *************
-    NSMutableArray *dataSource = [NSMutableArray array];
-    // rand() /((double)(RAND_MAX)/100) //取0-100中间的浮点数
-    double distanceMin = 0, distanceMax = 100;
-    double altitudeMin = 5.0, altitudeMax = 50;
-    double speedMin = 0.5, speedMax = 15;
-    
-    srand(time(NULL)); //Random seed
-    
-    for (int i=0; i< 11; i++) {
-        
-        RLLineChartItem *item = [[RLLineChartItem alloc] init];
-        double randVal;
-        
-        randVal = rand() /((double)(RAND_MAX)/distanceMax) + distanceMin;
-        item.xValue = randVal;
-        
-        randVal = rand() /((double)(RAND_MAX)/altitudeMax) + altitudeMin;
-        item.y1Value = randVal;
-        
-        randVal = rand() /((double)(RAND_MAX)/speedMax) + speedMin;
-        item.y2Value = randVal;
-        
-        NSLog(@"Random: item.xValue=%.2lf, item.y1Value=%.2lf, item.y2Value=%.2lf", item.xValue, item.y1Value, item.y2Value);
-        [dataSource addObject:item];
-    }
-    //************ End Test *********************
-    
-    ////////////// Create Line Char //////////////////////////
-    CGRect rect = CGRectMake(5, 40,
-                             self.view.frame.size.width - 5 - 5,
-                             300);
-    //    self.lineChartView = [[ARLineChartView alloc] initWithFrame:rect dataSource:dataSource xTitle:@"千米" y1Title:@"海拔（米）" y2Title:@"速度（千米/小时）" desc1:@"海拔" desc2:@"速度"];
-    self.lineChartView = [[ARLineChartView alloc] initWithFrame:rect dataSource:dataSource xTitle:@"Kilometre" y1Title:@"Altitude (meters)" y2Title:@"Speed (km/h)" desc1:@"Altitude" desc2:@"Speed"];
-    [self.view addSubview:self.lineChartView];
-    
-
+    [self loadData];
     
 }
 - (void)didReceiveMemoryWarning {
@@ -110,5 +67,86 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+//获取参数
+-(void) loadData{
+    AppDelegate *myDelegate = [[UIApplication sharedApplication]delegate];
+    //http://ps.leideng.org/index.php/User/App/showParameter.html?psid=1
+    NSString *urlString= [NSString stringWithFormat:@"%@/showParameter.html",myDelegate.ipString];
+    AFHTTPRequestOperationManager *manager=[AFHTTPRequestOperationManager manager];
+    // manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"application/json", nil];
+    manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"text/html", nil];
+    
+    // 请求参数
+    NSDictionary *parameters = @{ @"psid": [DataBaseNSUserDefaults getData:@"selectedPS"]
+                                  };
+    [manager POST:urlString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        
+        
+        NSString *result=[JsonUtil DataTOjsonString:responseObject];
+        NSLog(@"***************返回结果***********************");
+        //NSLog(result);
+        NSData *data=[result dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error=[[NSError alloc]init];
+        NSDictionary *doc= [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        if(doc!=nil){
+            NSLog(@"*****doc不为空***********");
+            //判断code 是不是0
+            if([@"0" isEqualToString:[doc objectForKey:@"code"]])
+            {
+                
+                //[self.lineChartView removeFromSuperview];
+                RLLineChartItem *item = [[RLLineChartItem alloc] init];
+                double randVal;
+                
+                //randVal = rand() /((double)(RAND_MAX)/distanceMax) + distanceMin;
+                item.xValue = i;
+                
+                
+                item.y1Value = i*2;
+                
+                
+                item.y2Value = i*3+50;
+                
+                
+                [dataSource addObject:item];
+                
+                if(!isChartAddedToSuperView){
+                    self.lineChartView = [[ARLineChartView alloc] initWithFrame:rect dataSource:dataSource xTitle:@"Kilometre" y1Title:@"Altitude (meters)" y2Title:@"Speed (km/h)" desc1:@"Altitude" desc2:@"Speed"];
+                    [self.view addSubview:self.lineChartView];
+                    isChartAddedToSuperView=true;
+                }else{
+                    [self.lineChartView refreshData:dataSource];
+                }
+                //模拟1秒后（
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    [self loadData];
+                    i++;
+                });
+            }
+            else{
+                [Alert showMessageAlert:[doc objectForKey:@"msg"] view:self];
+            }
+        }
+        else
+            NSLog(@"*****doc空***********");
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSString *errorUser=[error.userInfo objectForKey:NSLocalizedDescriptionKey];
+        if(error.code==-1009)
+            errorUser=@"主人，似乎没有网络喔！";
+        [Alert showMessageAlert:errorUser view:self];
+        //模拟1秒后（
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self loadData];
+        });
+        
+    }];
+    
+    
+}
+
 
 @end
